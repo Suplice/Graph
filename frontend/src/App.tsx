8ffcf6aced33a4e62bfe08cca7a28ef4495a6c95
Mateusz Duma <mateuszsuplice@gmail.com>
2024-09-13 01:14:46 +0200

    Implemented background gradient

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,14 +8,16 @@ function App() {
   const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
 
   return (
-    <div className=" w-9/12 mx-auto">
-      <Router>
-        <Layout isLoggedIn={isLoggedIn}>
-          <Routes>
-            <Route path="/" element={<Landing />}></Route>
-          </Routes>
-        </Layout>
-      </Router>
+    <div className="background">
+      <div className=" w-9/12 mx-auto z-20  bg-[radial-gradient(circle, rgba(112,132,200,0.2) 20%, rgba(21,0,210,0) 30%) 100px 100px, radial-gradient(circle, rgba(150,180,250,0.2) 20%, rgba(21,0,210,0) 40%) 500px 300px, radial-gradient(circle, rgba(100,150,250,0.15) 0%, rgba(0,0,0,0) 50%) 200px 100px]">
+        <Router>
+          <Layout isLoggedIn={isLoggedIn}>
+            <Routes>
+              <Route path="/" element={<Landing />}></Route>
+            </Routes>
+          </Layout>
+        </Router>
+      </div>
     </div>
   );
 }
