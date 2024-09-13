0dcf818638d2b839be3edf5d29921f647ad1a541
Mateusz Duma <mateuszsuplice@gmail.com>
2024-09-13 18:52:35 +0200

    Changed GuestNavbar functionality and made it more responsible

--- frontend/src/Components/GuestNavbar.tsx
+++ frontend/src/Components/GuestNavbar.tsx
@@ -1,25 +1,62 @@
 import React from "react";
 import { VscGraphLine } from "react-icons/vsc";
+import { NavLink } from "react-router-dom";
 
 const GuestNavbar: React.FC = () => {
   return (
-    <div className="flex flex-wrap items-center justify-around pb-16 pt-12 flex-col  lg:flex-row gap-4">
+    <div className="flex flex-wrap items-center justify-center pb-16 pt-12 flex-col  lg:flex-row gap-4 lg:justify-center 2xl:justify-between ">
       <div className="flex items-center space-x-4 ">
         <VscGraphLine size={60} />
         <p className="text-4xl font-semibold">Grapher</p>
       </div>
-      <ul className="flex lg:space-x-16 justify-center items-center text-2xl flex-wrap flex-col lg:flex-row lg:mx-4 ">
-        <li>
-          <a href="#home">Home</a>
+      <ul className="flex lg:space-x-16 justify-center items-center text-2xl flex-wrap flex-col lg:flex-row lg:mx-4">
+        <li className="relative">
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              isActive
+                ? " font-semibold hover:text-[1.75rem] transition-all duration-200 ease-in-out block"
+                : " hover:text-[1.75rem] transition-all duration-200 ease-in-out block"
+            }
+          >
+            Home
+          </NavLink>
         </li>
-        <li>
-          <a href="#about">About</a>
+        <li className="relative">
+          <NavLink
+            to="/About"
+            className={({ isActive }) =>
+              isActive
+                ? " font-semibold hover:text-[1.75rem] transition-all duration-200 ease-in-out block"
+                : " hover:text-[1.75rem] transition-all duration-200 ease-in-out block"
+            }
+          >
+            About
+          </NavLink>
         </li>
-        <li>
-          <a href="#getstarted">Get Started</a>
+        <li className="relative">
+          <NavLink
+            to="/GetStarted"
+            className={({ isActive }) =>
+              isActive
+                ? " font-semibold hover:text-[1.75rem] transition-all duration-200 ease-in-out block"
+                : " hover:text-[1.75rem] transition-all duration-200 ease-in-out block"
+            }
+          >
+            Get Started
+          </NavLink>
         </li>
-        <li>
-          <a href="#contact">Contact</a>
+        <li className="relative">
+          <NavLink
+            to="/Contact"
+            className={({ isActive }) =>
+              isActive
+                ? " font-semibold hover:text-[1.75rem] transition-all duration-200 ease-in-out block"
+                : " hover:text-[1.75rem] transition-all duration-200 ease-in-out block"
+            }
+          >
+            Contact
+          </NavLink>
         </li>
       </ul>
       <div className="flex lg:space-x-4 text-lgw font-mono flex-col lg:flex-row ">
