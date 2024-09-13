befb9603a5c8adf76726b03105f85e39dceb3a5e
Mateusz Duma <mateuszsuplice@gmail.com>
2024-09-13 16:13:05 +0200

    Guest bar is now responsive based on screen-width

--- frontend/src/Components/GuestNavbar.tsx
+++ frontend/src/Components/GuestNavbar.tsx
@@ -3,12 +3,12 @@ import { VscGraphLine } from "react-icons/vsc";
 
 const GuestNavbar: React.FC = () => {
   return (
-    <div className="flex flex-wrap items-center justify-between pb-16 pt-12  ">
+    <div className="flex flex-wrap items-center justify-between pb-16 pt-12 flex-col  lg:flex-row gap-4">
       <div className="flex items-center space-x-4 ">
         <VscGraphLine size={60} />
         <p className="text-4xl font-semibold">Grapher</p>
       </div>
-      <ul className="flex space-x-12 text-2xl antialiased ">
+      <ul className="flex lg:space-x-16 justify-center items-center text-2xl flex-wrap flex-col lg:flex-row lg:mx-4 ">
         <li>
           <a href="#home">Home</a>
         </li>
@@ -22,7 +22,7 @@ const GuestNavbar: React.FC = () => {
           <a href="#contact">Contact</a>
         </li>
       </ul>
-      <div className="flex space-x-4 text-lgw font-mono">
+      <div className="flex lg:space-x-4 text-lgw font-mono flex-col lg:flex-row ">
         <button className="px-4 py-2 rounded text-3xl">Sign In</button>
         <button className="bg-blue-500 text-white px-5 py-3 rounded-xl text-3xl hover:bg-blue-600 transition-colors duration-300">
           Sign Up
