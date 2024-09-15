ca2332cbe363ce4ec12874ed173b63108e1a39c0
Mateusz Duma <mateuszsuplice@gmail.com>
2024-09-15 20:43:18 +0200

    Using NavLink now causes automatic window scroll to the top

--- frontend/src/Components/Layout.tsx
+++ frontend/src/Components/Layout.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useEffect } from "react";
 import GuestNavbar from "./GuestNavbar";
 import MainFooter from "./MainFooter";
+import { useLocation } from "react-router-dom";
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -8,6 +9,12 @@ interface LayoutProps {
 }
 
 const Layout: React.FC<LayoutProps> = ({ children, isLoggedIn }) => {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
+
   return (
     <div>
       {isLoggedIn ? <GuestNavbar></GuestNavbar> : <GuestNavbar></GuestNavbar>}
