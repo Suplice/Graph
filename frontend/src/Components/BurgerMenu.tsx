9e10d21b942861b4fc266576a39db99d3ad7e2b1
Mateusz Duma <mateuszsuplice@gmail.com>
2024-09-17 23:31:49 +0200

    Implmented Burger menu for GuestNavbar, now when width changes overflowing elements are put in burger menu

---
+++ frontend/src/Components/BurgerMenu.tsx
@@ -0,0 +1,115 @@
+import React, { useState, useRef, useEffect } from "react";
+import { RiMenu3Line } from "react-icons/ri";
+import { NavLink } from "react-router-dom";
+
+interface BurgerMenuProps {
+  status: "none" | "partial" | "full";
+}
+
+const BurgerMenu: React.FC<BurgerMenuProps> = ({ status }) => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const menuRef = useRef<HTMLDivElement>(null);
+  const iconRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        menuRef.current &&
+        !menuRef.current.contains(event.target as Node) &&
+        !iconRef.current?.contains(event.target as Node)
+      ) {
+        setIsVisible(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  return (
+    <div className={`${status === "none" ? "hidden" : "relative"}`}>
+      <div ref={iconRef} className="">
+        <RiMenu3Line
+          size={40}
+          onClick={() => {
+            setIsVisible(!isVisible);
+          }}
+          className="cursor-pointer"
+        />
+      </div>
+      <div
+        ref={menuRef}
+        className={`absolute w-48 border transform -translate-x-[156px] rounded-lg text-center z-40 pt-2 bg-slate-200  ${isVisible ? "absolute" : "hidden"}`}
+      >
+        <ul className="flex flex-col gap-2">
+          <li className={`${status === "full" ? "" : "hidden"}`}>
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                isActive ? "font-bold" : "font-serif"
+              }
+            >
+              Home
+            </NavLink>
+          </li>
+          <li className={`${status === "full" ? "" : "hidden"}`}>
+            <NavLink
+              to="/About"
+              className={({ isActive }) =>
+                isActive ? "font-bold" : "font-serif"
+              }
+            >
+              About
+            </NavLink>
+          </li>
+          <li className={`${status === "full" ? "" : "hidden"}`}>
+            <NavLink
+              to="/GetStarted"
+              className={({ isActive }) =>
+                isActive ? "font-bold" : "font-serif"
+              }
+            >
+              Get Started
+            </NavLink>
+          </li>
+          <li className={`${status === "full" ? "" : "hidden"}`}>
+            <NavLink
+              to="/Contact"
+              className={({ isActive }) =>
+                isActive ? "font-bold" : "font-serif"
+              }
+            >
+              Contact
+            </NavLink>
+          </li>
+          <li
+            className={`${status === "partial" || status === "full" ? "" : "hidden"}`}
+          >
+            <NavLink
+              to="/SignIn"
+              className=" block font-mono mt-1 border-diminished-t pt-2"
+            >
+              Sign In
+            </NavLink>
+          </li>
+          <li
+            className={`${status === "partial" || status === "full" ? "" : "hidden"}`}
+          >
+            <NavLink
+              to="/Register"
+              className="hover:bg-blue-600 block font-mono text-white bg-blue-500 rounded-md p-1 "
+            >
+              Sign Up
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default BurgerMenu;
--- frontend/src/Components/GuestNavbar.tsx
+++ frontend/src/Components/GuestNavbar.tsx
@@ -1,15 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { VscGraphLine } from "react-icons/vsc";
 import { NavLink } from "react-router-dom";
+import BurgerMenu from "./BurgerMenu";
 
 const GuestNavbar: React.FC = () => {
+  const [status, setStatus] = useState<"none" | "partial" | "full">("none");
+
+  useEffect(() => {
+    const handleStatusChange = () => {
+      const currentWidth = window.innerWidth;
+
+      console.log(currentWidth);
+
+      if (currentWidth >= 1536) {
+        setStatus("none");
+      } else if (currentWidth >= 1124) {
+        setStatus("partial");
+      } else {
+        setStatus("full");
+      }
+    };
+
+    window.addEventListener("resize", handleStatusChange);
+
+    handleStatusChange();
+
+    return () => window.removeEventListener("resize", handleStatusChange);
+  }, []);
+
   return (
-    <div className="flex flex-wrap items-center justify-center pb-16 pt-12 flex-col  lg:flex-row gap-4 lg:justify-center 2xl:justify-between ">
+    <div className="flex flex-wrap items-center  pb-16 pt-12  lg:flex-row gap-4 justify-between ">
       <div className="flex items-center space-x-4 ">
-        <VscGraphLine size={60} />
-        <p className="text-4xl font-semibold">Grapher</p>
+        <VscGraphLine className="size-12 md:size-16" />
+        <p className="text-2xl md:text-3xl lg:text-4xl font-semibold">
+          Grapher
+        </p>
       </div>
-      <ul className="flex lg:space-x-16 justify-center items-center text-2xl flex-wrap flex-col lg:flex-row lg:mx-4">
+      <ul className="hidden lg:space-x-16 justify-center items-center text-2xl flex-wrap flex-col  xlg:flex-row xlg:flex lg:mx-4">
         <li className="relative">
           <NavLink
             to="/"
@@ -59,12 +86,14 @@ const GuestNavbar: React.FC = () => {
           </NavLink>
         </li>
       </ul>
-      <div className="flex lg:space-x-4 text-lgw font-mono flex-col lg:flex-row ">
+      <div className="lg:space-x-4 text-lg font-mono lg:flex-row hidden 2xl:flex ">
         <button className="px-4 py-2 rounded text-3xl">Sign In</button>
         <button className="bg-blue-500 text-white px-5 py-3 rounded-xl text-3xl hover:bg-blue-600 transition-colors duration-300">
           Sign Up
         </button>
       </div>
+
+      <BurgerMenu status={status} />
     </div>
   );
 };
