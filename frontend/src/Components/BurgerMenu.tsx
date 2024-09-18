ff7e2aba1ccf7d36fffec001429b1905e9169394
Mateusz Duma <mateuszsuplice@gmail.com>
2024-09-18 18:19:47 +0200

    Made slight visual changes in burger menu options

--- frontend/src/Components/BurgerMenu.tsx
+++ frontend/src/Components/BurgerMenu.tsx
@@ -43,7 +43,7 @@ const BurgerMenu: React.FC<BurgerMenuProps> = ({ status }) => {
       </div>
       <div
         ref={menuRef}
-        className={`absolute w-48 border transform -translate-x-[156px] rounded-lg text-center z-40 pt-2 bg-slate-200  ${isVisible ? "absolute" : "hidden"}`}
+        className={`absolute w-48 border transform -translate-x-[156px] rounded-lg text-center z-40 ${status === "partial" ? "" : "pt-2"}  bg-slate-200  ${isVisible ? "absolute" : "hidden"}`}
       >
         <ul className="flex flex-col gap-2">
           <li className={`${status === "full" ? "" : "hidden"}`}>
@@ -91,17 +91,17 @@ const BurgerMenu: React.FC<BurgerMenuProps> = ({ status }) => {
           >
             <NavLink
               to="/SignIn"
-              className=" block font-mono mt-1 border-diminished-t pt-2"
+              className={` block font-mono  ${status === "partial" ? "hover:bg-slate-50 rounded-md p-1" : "border-diminished-t pt-2 mt-1 "}  transition-colors`}
             >
               Sign In
             </NavLink>
           </li>
           <li
-            className={`${status === "partial" || status === "full" ? "" : "hidden"}`}
+            className={`${status === "partial" || status === "full" ? "" : "hidden"} `}
           >
             <NavLink
               to="/Register"
-              className="hover:bg-blue-600 block font-mono text-white bg-blue-500 rounded-md p-1 "
+              className="hover:bg-blue-600 block font-mono text-white bg-blue-500 rounded-md p-1 transition-colors"
             >
               Sign Up
             </NavLink>
