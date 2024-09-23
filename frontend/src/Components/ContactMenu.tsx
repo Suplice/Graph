083cefb56b820d71e6289ac8790a3ad69bdee1b8
Mateusz Duma <mateuszsuplice@gmail.com>
2024-09-23 18:51:38 +0200

    refactor: The logic of contact via messages has been separated into a separate component to facilitate operation

---
+++ frontend/src/Components/ContactMenu.tsx
@@ -0,0 +1,84 @@
+import react, { useState } from "react";
+import { PhoneInput } from "react-international-phone";
+
+const ContactMenu: React.FC = () => {
+  const [phone, setPhone] = useState("");
+  const [isFocused, setIsFocused] = useState(false);
+  const [characterCount, setCharacterCount] = useState(0);
+
+  return (
+    <div className="w-full md:w-3/5 flex flex-row flex-wrap ">
+      <div className="w-full md:w-1/2 px-1 lg:pr-4 xl:pr-10">
+        <label className="font-semibold pl-2">First Name</label>
+        <input
+          type="text"
+          className="w-full border-2 rounded-xl border-gray-300 p-2 lg:p-3 focus:outline-none focus:border-slate-400 focus:placeholder-slate-500"
+          placeholder="First Name"
+        ></input>
+        <p className="text-sm text-red-500 pl-2 ">error</p>
+      </div>
+
+      <div className="w-full md:w-1/2 px-1 lg:pr-4 xl:pr-10 md:mt-0 mt-2">
+        <label className="font-semibold pl-2 h-5">Last Name</label>
+        <input
+          type="text"
+          className="w-full border-2 rounded-xl border-gray-300 p-2 lg:p-3 focus:outline-none focus:border-slate-400 focus:placeholder-slate-500"
+          placeholder="Last Name"
+        ></input>
+        <p className="text-sm text-red-500 pl-2 h-5">error</p>
+      </div>
+
+      <div className="w-full px-1 lg:pr-4 xl:pr-10">
+        <label className="font-semibold pl-2">Email Addres</label>
+        <input
+          type="text"
+          className="w-full border-2 rounded-xl border-gray-300 p-2 focus:outline-none focus:border-slate-400 focus:placeholder-slate-500"
+          placeholder="you@example.com"
+        ></input>
+        <p className="text-sm text-red-500 pl-2 h-5">error</p>
+      </div>
+
+      <div className="w-full px-1 lg:pr-4  xl:pr-10">
+        <label className="font-semibold pl-2">Phone Number</label>
+        <PhoneInput
+          onFocus={() => setIsFocused(true)}
+          onBlur={() => setIsFocused(false)}
+          defaultCountry="pl"
+          value={phone}
+          onChange={(phone) => setPhone(phone)}
+          className={`w-full border-2 rounded-xl border-gray-300 p-1 focus:outline-none bg-white ${isFocused ? "border-slate-500" : "border-gray-300"}`}
+          inputClassName="customInput"
+          countrySelectorStyleProps={{
+            buttonClassName: "countrySelectorButton",
+          }}
+        />
+        <p className="text-sm text-red-500 pl-2 h-5">error</p>
+      </div>
+
+      <div className="w-full px-1 lg:pr-4  xl:pr-10 ">
+        <label className="font-semibold pl-2">Message</label>
+        <textarea
+          className="w-full border-2 rounded-xl border-gray-300 p-2 focus:outline-none focus:border-slate-400 focus:placeholder-slate-500 scrollbar-none resize-none"
+          placeholder="Leave us a message..."
+          rows={5}
+          maxLength={200} // Set a maximum length for the message
+          onChange={(e) => setCharacterCount(e.target.value.length)}
+        />
+        <div className="flex flex-row">
+          <p className="text-sm text-red-500 -translate-y-1 pl-2 h-5 w-1/2"></p>
+          <p className="text-right -translate-y-1  font-normal text-sm md:text-base w-1/2">
+            ({characterCount}/200)
+          </p>
+        </div>
+      </div>
+
+      <div className="w-full px-1 lg:pr-4 lg:mt-4 xl:pr-10">
+        <button className="w-full bg-slate-900 text-white hover:bg-slate-800 duration-300  transition-colors rounded-lg py-3 mt-5 font-sans text-md">
+          Send Message
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default ContactMenu;
--- frontend/src/Pages/Contact/Contact.tsx
+++ frontend/src/Pages/Contact/Contact.tsx
@@ -1,5 +1,4 @@
-import React, { useState } from "react";
-import { PhoneInput } from "react-international-phone";
+import React from "react";
 import { RiWechatLine } from "react-icons/ri";
 import { PiXLogoBold } from "react-icons/pi";
 import { PiXLogoFill } from "react-icons/pi";
@@ -8,12 +7,9 @@ import { BsInstagram } from "react-icons/bs";
 import { FaFacebook } from "react-icons/fa";
 import "react-international-phone/style.css";
 import "./Contact.css";
+import ContactMenu from "../../Components/ContactMenu";
 
 const Contact: React.FC = () => {
-  const [phone, setPhone] = useState("");
-  const [isFocused, setIsFocused] = useState(false);
-  const [characterCount, setCharacterCount] = useState(0);
-
   return (
     <>
       <div className=" flex flex-col items-center min-h-screen">
@@ -31,78 +27,7 @@ const Contact: React.FC = () => {
           </p>
         </div>
         <div className="my-10 md:my-14 lg:my-16 xl:my-20 flex flex-col md:flex-row gap-10  w-full md:w-3/4 ">
-          <div className="w-full md:w-3/5 flex flex-row flex-wrap ">
-            <div className="w-full md:w-1/2 px-1 lg:pr-4 xl:pr-10">
-              <label className="font-semibold pl-2">First Name</label>
-              <input
-                type="text"
-                className="w-full border-2 rounded-xl border-gray-300 p-2 lg:p-3 focus:outline-none focus:border-slate-400 focus:placeholder-slate-500"
-                placeholder="First Name"
-              ></input>
-              <p className="text-sm text-red-500 pl-2 ">error</p>
-            </div>
-
-            <div className="w-full md:w-1/2 px-1 lg:pr-4 xl:pr-10 md:mt-0 mt-2">
-              <label className="font-semibold pl-2 h-5">Last Name</label>
-              <input
-                type="text"
-                className="w-full border-2 rounded-xl border-gray-300 p-2 lg:p-3 focus:outline-none focus:border-slate-400 focus:placeholder-slate-500"
-                placeholder="Last Name"
-              ></input>
-              <p className="text-sm text-red-500 pl-2 h-5">error</p>
-            </div>
-
-            <div className="w-full px-1 lg:pr-4 xl:pr-10">
-              <label className="font-semibold pl-2">Email Addres</label>
-              <input
-                type="text"
-                className="w-full border-2 rounded-xl border-gray-300 p-2 focus:outline-none focus:border-slate-400 focus:placeholder-slate-500"
-                placeholder="you@example.com"
-              ></input>
-              <p className="text-sm text-red-500 pl-2 h-5">error</p>
-            </div>
-
-            <div className="w-full px-1 lg:pr-4  xl:pr-10">
-              <label className="font-semibold pl-2">Phone Number</label>
-              <PhoneInput
-                onFocus={() => setIsFocused(true)}
-                onBlur={() => setIsFocused(false)}
-                defaultCountry="pl"
-                value={phone}
-                onChange={(phone) => setPhone(phone)}
-                className={`w-full border-2 rounded-xl border-gray-300 p-1 focus:outline-none bg-white ${isFocused ? "border-slate-500" : "border-gray-300"}`}
-                inputClassName="customInput"
-                countrySelectorStyleProps={{
-                  buttonClassName: "countrySelectorButton",
-                }}
-              />
-              <p className="text-sm text-red-500 pl-2 h-5">error</p>
-            </div>
-
-            <div className="w-full px-1 lg:pr-4  xl:pr-10 ">
-              <label className="font-semibold pl-2">Message</label>
-              <textarea
-                className="w-full border-2 rounded-xl border-gray-300 p-2 focus:outline-none focus:border-slate-400 focus:placeholder-slate-500 scrollbar-none resize-none"
-                placeholder="Leave us a message..."
-                rows={5}
-                maxLength={200} // Set a maximum length for the message
-                onChange={(e) => setCharacterCount(e.target.value.length)}
-              />
-              <div className="flex flex-row">
-                <p className="text-sm text-red-500 -translate-y-1 pl-2 h-5 w-1/2"></p>
-                <p className="text-right -translate-y-1  font-normal text-sm md:text-base w-1/2">
-                  ({characterCount}/200)
-                </p>
-              </div>
-            </div>
-
-            <div className="w-full px-1 lg:pr-4 lg:mt-4 xl:pr-10">
-              <button className="w-full bg-slate-900 text-white hover:bg-slate-800 duration-300  transition-colors rounded-lg py-3 mt-5 font-sans text-md">
-                Send Message
-              </button>
-            </div>
-          </div>
-
+          <ContactMenu />
           <div className="md:w-2/5 w-full ">
             <div className="w-full flex flex-col">
               <h1 className="text-3xl font-bold mb-2">Chat with us</h1>
