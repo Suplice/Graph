5b24ba43010fc696e2c912c57b2db1637979a589
Mateusz Duma <mateuszsuplice@gmail.com>
2024-09-26 00:07:42 +0200

    feat: Allowed requests from frontend. Added authentication path

--- backend/functions/src/index.ts
+++ backend/functions/src/index.ts
@@ -2,12 +2,26 @@ import * as functions from "firebase-functions";
 import express from "express";
 import cors from "cors";
 import * as admin from "firebase-admin";
+import authRoutes from "./routes/authRoutes";
 
 admin.initializeApp();
 
 const app = express();
 
-app.use(cors({ origin: true }));
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
 app.use(express.json());
 
-exports.api = functions.https.onRequest(app);
+app.use("/auth", authRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Hello from Express!");
+});
+
+exports.api = functions.region("europe-central2").https.onRequest(app);
