8c22d23941e3e777652ce60fb4db9c12c8ee5862
Mateusz Duma <mateuszsuplice@gmail.com>
2024-09-27 22:51:30 +0200

    feat: Add registerValidator middleware and handleValidationErrors function

--- backend/functions/src/routes/authRoutes.ts
+++ backend/functions/src/routes/authRoutes.ts
@@ -1,8 +1,17 @@
 import express from "express";
 import authController from "../controllers/authController";
+import {
+  handleValidationErrors,
+  registerValidator,
+} from "../middlewares/authValidators/registerValidator";
 
 const router = express.Router();
 
-router.post("/register", authController.registerUser);
+router.post(
+  "/register",
+  registerValidator,
+  handleValidationErrors,
+  authController.registerUser
+);
 
 export default router;
