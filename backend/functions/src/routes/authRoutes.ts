7fb65dd337d18d07df205c27330e0ec903e308f4
Mateusz Duma <mateuszsuplice@gmail.com>
2024-09-28 23:24:10 +0200

    feat: Add baseLoginValidator middleware for login validation in authRoutes

--- backend/functions/src/routes/authRoutes.ts
+++ backend/functions/src/routes/authRoutes.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import authController from "../controllers/authController";
 import { validateRegistration } from "../middlewares/authValidators/baseRegisterValidator";
+import { validateLogin } from "../middlewares/authValidators/baseLoginValidator";
 
 const router = express.Router();
 
 router.post("/register", validateRegistration, authController.registerUser);
+router.post("/login", validateLogin);
 
 export default router;
