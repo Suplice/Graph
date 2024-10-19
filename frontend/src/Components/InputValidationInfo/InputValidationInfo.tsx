51c9ec6febcd4e344f86a935b8fc83691d22b6b6
Mateusz Duma <mateuszsuplice@gmail.com>
2024-10-19 23:40:58 +0200

    refactor: Update InputValidationInfo component to display validation results

--- frontend/src/Components/InputValidationInfo/InputValidationInfo.tsx
+++ frontend/src/Components/InputValidationInfo/InputValidationInfo.tsx
@@ -1,29 +1,34 @@
 import React from "react";
+import { FcCheckmark } from "react-icons/fc";
+import { FcHighPriority } from "react-icons/fc";
 
 interface InputValidationInfoProps {
-  direction: "left" | "right";
+  validationResults: { message: string; isValid: boolean }[];
 }
 
 const InputValidationInfo: React.FC<InputValidationInfoProps> = ({
-  direction,
+  validationResults,
 }) => {
   return (
     <div
-      className={`absolute border  p-2 text-sm bg-white shadow-md rounded-md z-10 w-fit
-        
-      `}
+      className={`absolute border p-2 text-sm bg-white shadow-md rounded-md z-10 w-fit`}
     >
-      <p>dddddddddddddd</p>
-      <p>dddddddddddddd</p>
-      <p>dddddddddddddd</p>
-      <p>dddddddddddddd</p>
-      <p>dddddddddddddd</p>
-      <p>dddddddddddddd</p>
-      <p>dddddddddddddd</p>
-      <p>dddddddddddddd</p>
-      <p>dddddddddddddd</p>
-      <p>dddddddddddddd</p>
-      <p>dddddddddddddd</p>
+      {validationResults.map((result, index) => (
+        <div key={index} className="flex items-center w-fit h-8">
+          {result.isValid ? (
+            <FcCheckmark className="lg:min-w-6 lg:min-h-6 min-h-3 min-w-3" />
+          ) : (
+            <FcHighPriority className="lg:min-w-6 lg:min-h-6 min-h-3 min-w-3" />
+          )}
+          <p
+            className={
+              result.isValid ? "text-green-500 ml-2" : "text-red-500 ml-2"
+            }
+          >
+            {result.message}
+          </p>
+        </div>
+      ))}
     </div>
   );
 };
