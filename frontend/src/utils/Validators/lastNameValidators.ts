a0498f25adcc6cfa22335bd9a78d28b40ce7c032
Mateusz Duma <mateuszsuplice@gmail.com>
2024-10-24 17:33:26 +0200

    refactor: Update error messages for last name validation

--- frontend/src/utils/Validators/lastNameValidators.ts
+++ frontend/src/utils/Validators/lastNameValidators.ts
@@ -1,9 +1,9 @@
 export const validateLastNameIsReq = (lastName: string) => ({
-  message: "First Name is required",
+  message: "Last Name is required",
   isValid: !lastName ? false : true,
 });
 
 export const validateLastNameIsMinLength = (lastName: string) => ({
-  message: "First Name must be at least 2 characters",
+  message: "Last Name must be at least 2 characters",
   isValid: lastName.length >= 2 ? true : false,
 });
