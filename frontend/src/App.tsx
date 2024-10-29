f71ce151ba1a579ccead167d64548860674c3051
Mateusz Duma <mateuszsuplice@gmail.com>
2024-10-29 22:09:26 +0100

    Refactor: Update App component to use AuthProvider and Main component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,14 @@
-import React, { useState } from "react";
-import Landing from "./Pages/Landing/Landing";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
-import Layout from "./Components/Layout/Layout";
-import About from "./Pages/About/About";
-import Contact from "./Pages/Contact/Contact";
-import SignIn from "./Pages/SignIn/SignIn";
-import SignUp from "./Pages/SignUp/SignUp";
 import { AuthProvider } from "./Context/AuthContext";
 import Main from "./Components/Main/Main";
+import { getAuth } from "firebase/auth";
 
 function App() {
   return (
     <AuthProvider>
-      <div
-        className="background mx-auto w-full min-h-screen bg-cover bg-center"
-        style={{ backgroundImage: "url(/Images/bg.jpg)" }}
-      >
-        <div className="w-9/12 mx-auto z-20">
-          <Router>
-            <Main />
-          </Router>
-        </div>
-      </div>
+      <Main />
     </AuthProvider>
   );
 }
