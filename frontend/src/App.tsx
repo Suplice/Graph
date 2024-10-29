d6c6e30da8e2731520ce850ae141ab365ed7406e
Mateusz Duma <mateuszsuplice@gmail.com>
2024-10-29 20:48:13 +0100

    Refactor: Update App component to use AuthProvider and Main component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,30 +7,23 @@ import About from "./Pages/About/About";
 import Contact from "./Pages/Contact/Contact";
 import SignIn from "./Pages/SignIn/SignIn";
 import SignUp from "./Pages/SignUp/SignUp";
+import { AuthProvider } from "./Context/AuthContext";
+import Main from "./Components/Main/Main";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
-
   return (
-    <div
-      className="background mx-auto w-full min-h-screen bg-cover bg-center"
-      style={{ backgroundImage: "url(/Images/bg.jpg)" }}
-    >
-      <div className="w-9/12 mx-auto z-20">
-        <Router>
-          <Layout isLoggedIn={isLoggedIn}>
-            <Routes>
-              <Route path="/" element={<Landing />}></Route>
-              <Route path="/Contact" element={<Contact />}></Route>
-              <Route path="/GetStarted" element={<SignIn />}></Route>
-              <Route path="/About" element={<About />}></Route>
-              <Route path="/SignIn" element={<SignIn />}></Route>
-              <Route path="/SignUp" element={<SignUp />}></Route>
-            </Routes>
-          </Layout>
-        </Router>
+    <AuthProvider>
+      <div
+        className="background mx-auto w-full min-h-screen bg-cover bg-center"
+        style={{ backgroundImage: "url(/Images/bg.jpg)" }}
+      >
+        <div className="w-9/12 mx-auto z-20">
+          <Router>
+            <Main />
+          </Router>
+        </div>
       </div>
-    </div>
+    </AuthProvider>
   );
 }
 
