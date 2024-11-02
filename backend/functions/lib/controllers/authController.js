8fdd6b4b0556e191c3b14d83af0be09f65a40944
Mateusz Duma <mateuszsuplice@gmail.com>
2024-11-02 23:43:46 +0100

    refactor: Remove console logs from authController for cleaner code

--- backend/functions/lib/controllers/authController.js
+++ backend/functions/lib/controllers/authController.js
@@ -33,14 +33,11 @@ const verifyToken = (continuePipeline) => async (req, res, next) => {
     var _a;
     // Get the token from the Authorization header
     const token = (_a = req.headers.authorization) === null || _a === void 0 ? void 0 : _a.split("Bearer ")[1];
-    console.log("Token:", token);
     // Check if token exists
     if (!token) {
-        console.log("No token provided");
         return res.status(401).json({ message: "No token provided" });
     }
     try {
-        console.log("Verifying token");
         // Verify the token using Firebase Admin SDK
         const decodedToken = await firebase_admin_1.default.auth().verifyIdToken(token);
         res.locals.uid = decodedToken.uid;
@@ -58,13 +55,10 @@ const verifyToken = (continuePipeline) => async (req, res, next) => {
 };
 const getUser = async (req, res) => {
     const { id } = req.params;
-    console.log("ID:", id);
     try {
-        console.log("Fetching user data");
         const user = await firebase_admin_1.default.firestore().collection("users").doc(id).get();
         if (!user.exists) {
             return res.status(404).json({ message: "User not found" });
-            console.log("User not found");
         }
         return res.status(200).json(user.data());
     }
