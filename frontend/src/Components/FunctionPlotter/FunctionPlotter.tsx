b94d6696fc75c888f9ad5c57f142d88501a52326
Mateusz Duma <mateuszsuplice@gmail.com>
2024-11-03 21:13:41 +0100

    feat: Create FunctionPlotter component for displaying function plots

---
+++ frontend/src/Components/FunctionPlotter/FunctionPlotter.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const FunctionPlotter: React.FC = () => {
+  return (
+    <div className=" p-4 flex h-full w-full ">
+      <h1> this is function plotter</h1>
+    </div>
+  );
+};
+
+export default FunctionPlotter;
