aa60e981078f343c4323c841d40ba1dda6b637e2
Mateusz Duma <mateuszsuplice@gmail.com>
2024-11-07 23:28:50 +0100

    feat: Add data fetching in StatisticsSummaryCard for user-specific statistics

--- frontend/src/Components/OverviewCards/StatisticsSummaryCard/StatisticsSummaryCard.tsx
+++ frontend/src/Components/OverviewCards/StatisticsSummaryCard/StatisticsSummaryCard.tsx
@@ -1,6 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 const StatisticsSummaryCard: React.FC = () => {
+  useEffect(() => {
+    const fetchData = async () => {
+      const id = localStorage.getItem("uid");
+
+      const res = await fetch(
+        process.env.React_APP_API_URL + `/api/statistics/${id}`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        }
+      );
+
+      const data = await res.json();
+
+      console.log(data);
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="bg-white rounded-xl shadow-lg p-6 w-full h-full border border-gray-200">
       <h2 className="text-2xl font-semibold text-gray-800 mb-6">
